42c3420ecdce0ae5ad25fe03754b728b35dd0e48
Chamath Heshan <chmth@protonmail.com>
2024-06-16 20:44:52 +1000

    Change to relative paths (from absolute)

--- generate_file_lists.py
+++ generate_file_lists.py
@@ -1,9 +1,9 @@
 import os
 
-def create_file_list(image_dir, output_file):
+def create_file_list(image_dir, output_file, relative_path_prefix):
     with open(output_file, 'w') as f:
         for image_name in os.listdir(image_dir):
-            image_path = os.path.join(image_dir, image_name)
+            image_path = os.path.join(relative_path_prefix, image_name).replace('\\', '/')
             f.write(f"{image_path}\n")
 
 train_image_dir = 'D:/Projects/Coding/deepvision/datasets/coco/train2017'
@@ -11,7 +11,7 @@ val_image_dir = 'D:/Projects/Coding/deepvision/datasets/coco/val2017'
 train_output_file = 'D:/Projects/Coding/deepvision/datasets/coco/train2017.txt'
 val_output_file = 'D:/Projects/Coding/deepvision/datasets/coco/val2017.txt'
 
-create_file_list(train_image_dir, train_output_file)
-create_file_list(val_image_dir, val_output_file)
+create_file_list(train_image_dir, train_output_file, 'images/train2017')
+create_file_list(val_image_dir, val_output_file, 'images/val2017')
 
 print("File lists created.")
